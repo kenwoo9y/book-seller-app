11d821ea26a28e5a29da5a9fb5c1f8769715fff0
kenwoo9y <ei.1px.9.01@gmail.com>
2022-04-04 23:48:29 +0900

    add backend

---
+++ backend/src/main/java/com/example/booksellerapp/BookSellerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksellerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSellerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSellerAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/booksellerapp/BookSellerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksellerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSellerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
