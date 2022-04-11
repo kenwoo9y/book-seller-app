f91f8ecf1a632c5a9730300fe48ac418bbf6b82d
kenwoo9y <ei.1px.9.01@gmail.com>
2022-04-11 23:51:54 +0900

    added User class

---
+++ backend/src/main/java/com/example/booksellerapp/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.booksellerapp.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String name;
+    private LocalDateTime createTime;
+}
