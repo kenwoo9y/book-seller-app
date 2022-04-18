359c0f460272fa81d759152e11253e806b1e017d
kenwoo9y <ei.1px.9.01@gmail.com>
2022-04-18 23:57:41 +0900

    add controller

---
+++ backend/src/main/java/com/example/booksellerapp/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.booksellerapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {}
