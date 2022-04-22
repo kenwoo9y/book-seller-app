a692867ea9bff5d8bd9bdd9058be900b3fadcf6f
kenwoo9y <ei.1px.9.01@gmail.com>
2022-04-22 23:56:51 +0900

    add BookController

---
+++ backend/src/main/java/com/example/booksellerapp/controller/BookController.java
@@ -0,0 +1,8 @@
+package com.example.booksellerapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {}
