c30c6680f6ae053c6904a4fdcf056893efcdf926
kenwoo9y <ei.1px.9.01@gmail.com>
2022-04-24 23:47:41 +0900

    add PurchaseRepository

---
+++ backend/src/main/java/com/example/booksellerapp/repository/PurchaceRepository.java
@@ -0,0 +1,8 @@
+package com.example.booksellerapp.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface PurchaceRepository {}
---
+++ backend/src/main/java/com/example/booksellerapp/repository/PurchaseRepositoryImpl.java
@@ -0,0 +1,3 @@
+package com.example.booksellerapp.repository;
+
+public class PurchaseRepositoryImpl implements PurchaceRepository {}
