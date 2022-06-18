ca078121bcb6507b4f6f221c9d26d3f14162c08b
kenwoo9y <ei.1px.9.01@gmail.com>
2022-06-18 23:26:02 +0900

    update schema

--- backend/src/main/resources/database/book_seller_schema.sql
+++ backend/src/main/resources/database/book_seller_schema.sql
@@ -2,6 +2,7 @@ create table book_seller_sc.user (
     id varchar(80) not null primary key,
     user_name varchar (80),
     password varchar(80),
+    email varchar(80),
     first_name varchar(80),
     last_name varchar(80),
     created_time timestamp,
