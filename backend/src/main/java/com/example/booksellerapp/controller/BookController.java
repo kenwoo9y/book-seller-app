af670d7f5d438020e53815fb9297c2e3e2ae5567
kenwoo9y <ei.1px.9.01@gmail.com>
2022-06-23 23:49:25 +0900

    update contorollers

--- backend/src/main/java/com/example/booksellerapp/controller/BookController.java
+++ backend/src/main/java/com/example/booksellerapp/controller/BookController.java
@@ -1,8 +1,39 @@
 package com.example.booksellerapp.controller;
 
+import com.example.booksellerapp.entity.Book;
+import com.example.booksellerapp.service.BookService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/book")
-public class BookController {}
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<?> saveBook(@RequestBody Book book) {
+        return new ResponseEntity<>(bookService.saveBook(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllBooks() {
+        return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{bookId}")
+    public ResponseEntity<?> deleteBookById(@PathVariable String bookId) {
+        bookService.deleteBookById(bookId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- backend/src/main/java/com/example/booksellerapp/controller/UserController.java
+++ backend/src/main/java/com/example/booksellerapp/controller/UserController.java
@@ -1,8 +1,30 @@
 package com.example.booksellerapp.controller;
 
+import java.nio.file.attribute.UserPrincipal;
+
+import com.example.booksellerapp.entity.Role;
+import com.example.booksellerapp.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/api/user")
-public class UserController {}
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/change/{role}")
+    public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Role role) {
+        userService.changeRole(userPrincipal.getUsername(), role);
+        
+        return ResponseEntity.ok(true);
+    }
+}
