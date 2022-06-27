8b730a1bc2c1dd919493231b0a7c91140cefd053
kenwoo9y <ei.1px.9.01@gmail.com>
2022-06-27 23:36:03 +0900

    remove unnecessary import

--- backend/src/main/java/com/example/booksellerapp/controller/UserController.java
+++ backend/src/main/java/com/example/booksellerapp/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
